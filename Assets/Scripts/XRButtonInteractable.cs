59f07880d8908897940d0e9e798f7a9c88d6f311
GabbiePurch <purchi375@morrisville.edu>
2026-02-25 13:27:43 -0500

    Initialized UI Combination Lock and event handling."

--- Assets/Scripts/XRButtonInteractable.cs
+++ Assets/Scripts/XRButtonInteractable.cs
@@ -7,21 +7,14 @@ using UnityEngine.UI;
 public class XRButtonInteractable : XRSimpleInteractable
 {
     [SerializeField] Image buttonImage;
-    [SerializeField] Color[] buttonColors = new Color[4];
-
-    private Color normalColor;
-    private Color highlightedColor;
-    private Color pressedColor;
-    private Color selectedColor;
+    [SerializeField] private Color normalColor;
+    [SerializeField] private Color highlightedColor;
+    [SerializeField] private Color pressedColor;
+    [SerializeField] private Color selectedColor;
     private bool isPressed;
     void Start()
     {
-        normalColor = buttonColors[0];
-        highlightedColor = buttonColors[1];
-        pressedColor = buttonColors[2];
-        selectedColor = buttonColors[3];
-
-        buttonImage.color = normalColor;
+        ResetColor();
     }
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
@@ -36,7 +29,7 @@ public class XRButtonInteractable : XRSimpleInteractable
 
         if (!isPressed)
         {
-            buttonImage.color = normalColor;
+            ResetColor();
         }
 
     }
@@ -53,8 +46,8 @@ public class XRButtonInteractable : XRSimpleInteractable
         buttonImage.color = selectedColor;
     }
 
-    void Update()
+    public void ResetColor()
     {
-        
+        buttonImage.color = normalColor;
     }
 }
