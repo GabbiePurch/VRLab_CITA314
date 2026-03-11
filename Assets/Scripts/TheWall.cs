fe7e5595ce7e5fe2cbd6f2d29779b00ce4bf5278
GabbiePurch <purchi375@morrisville.edu>
2026-03-11 12:20:50 -0400

    "Built the procedural wall using basic functions."

---
+++ Assets/Scripts/TheWall.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TheWall : MonoBehaviour
+{
+    [SerializeField] XRSocketInteractor wallSocket;
+    [SerializeField] GameObject[] wallCubes;
+    void Start()
+    {
+        if (wallSocket != null)
+        {
+            wallSocket.selectEntered.AddListener(OnSocketEntered);
+            wallSocket.selectExited.AddListener(OnSocketExited);
+        }
+    }
+
+    private void OnSocketExited(SelectExitEventArgs arg0)
+    {
+        for (int i = 0; i < wallCubes.Length; i++ )
+        {
+            if (wallCubes[i] != null)
+            {
+                Rigidbody rb = wallCubes[i].GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+            }
+        }
+    }
+
+    private void OnSocketEntered(SelectEnterEventArgs arg0)
+    {
+        for (int i = 0; i < wallCubes.Length; i++ )
+        {
+            if (wallCubes[i] != null)
+            {
+                Rigidbody rb = wallCubes[i].GetComponent<Rigidbody>();
+                rb.isKinematic = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
